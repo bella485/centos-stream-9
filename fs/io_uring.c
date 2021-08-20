70bc414220a3feedf118a5a3f90a34bd8b6f3c84
Jeff Moyer <jmoyer@redhat.com>
2021-08-21 06:19:54 +0800

    io_uring: fix lack of protection for compl_nr
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2068237
    Conflicts: RHEL is missing commit cd0ca2e048dc ("io_uring: inline
    struct io_comp_state"), so comp.nr is used in RHEL instead of the
    upstream compl_nr.
    
    commit 99c8bc52d1321ab3a711eba2941eadbe7425230f
    Author: Hao Xu <haoxu@linux.alibaba.com>
    Date:   Sat Aug 21 06:19:54 2021 +0800
    
        io_uring: fix lack of protection for compl_nr
        
        coml_nr in ctx_flush_and_put() is not protected by uring_lock, this
        may cause problems when accessing in parallel:
        
        say coml_nr > 0
        
          ctx_flush_and put                  other context
           if (compl_nr)                      get mutex
                                              coml_nr > 0
                                              do flush
                                                  coml_nr = 0
                                              release mutex
                get mutex
                   do flush (*)
                release mutex
        
        in (*) place, we call io_cqring_ev_posted() and users likely get
        no events there. To avoid spurious events, re-check the value when
        under the lock.
        
        Fixes: 2c32395d8111 ("io_uring: fix __tctx_task_work() ctx race")
        Signed-off-by: Hao Xu <haoxu@linux.alibaba.com>
        Link: https://lore.kernel.org/r/20210820221954.61815-1-haoxu@linux.alibaba.com
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1962,7 +1962,8 @@ static void ctx_flush_and_put(struct io_ring_ctx *ctx)
 		return;
 	if (ctx->submit_state.comp.nr) {
 		mutex_lock(&ctx->uring_lock);
-		io_submit_flush_completions(ctx);
+		if (ctx->submit_state.comp.nr)
+			io_submit_flush_completions(ctx);
 		mutex_unlock(&ctx->uring_lock);
 	}
 	percpu_ref_put(&ctx->refs);
