torvalds/linux

commit 70536bf4eb07ed5d2816ccb274e5e6b41b95a437

NFS: Clean up reset of the mirror accounting variables
Now that nfs_pageio_do_add_request() resets the pg_count, we don't need
these other inlined resets.

Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

fs/nfs/pagelist.c
@@ -1132,12 +1132,8 @@ static void nfs_pageio_doio(struct nfs_pageio_descriptor *desc)
 		int error = desc->pg_ops->pg_doio(desc);
 		if (error < 0)
 			desc->pg_error = error;
-		if (list_empty(&mirror->pg_list)) {
+		if (list_empty(&mirror->pg_list))
 			mirror->pg_bytes_written += mirror->pg_count;
-			mirror->pg_count = 0;
-			mirror->pg_base = 0;
-			mirror->pg_recoalesce = 0;
-		}
 	}
 }
 
@@ -1227,9 +1223,6 @@ static int nfs_do_recoalesce(struct nfs_pageio_descriptor *desc)
 
 	do {
 		list_splice_init(&mirror->pg_list, &head);
-		mirror->pg_count = 0;
-		mirror->pg_base = 0;
-		mirror->pg_recoalesce = 0;
 
 		while (!list_empty(&head)) {
 			struct nfs_page *req;

bella485/centos-stream-9

commit 53b360b9358ed204e58439e2ac952c02ef4813bd

Subject: NFS: Don't loop forever in nfs_do_recoalesce()
Bugzilla: http://bugzilla.redhat.com/2069274

commit d02d81efc7564b4d5446a02e0214a164cf00b1f3
Author: Trond Myklebust <trond.myklebust@hammerspace.com>
Date:   Fri Mar 25 21:51:03 2022 -0400

    NFS: Don't loop forever in nfs_do_recoalesce()

    If __nfs_pageio_add_request() fails to add the request, it will return
    with either desc->pg_error < 0, or mirror->pg_recoalesce will be set, so
    we are guaranteed either to exit the function altogether, or to loop.

    However if there is nothing left in mirror->pg_list to coalesce, we must
    exit, so make sure that we clear mirror->pg_recoalesce every time we
    loop.

    Reported-by: Olga Kornievskaia <aglo@umich.edu>
    Fixes: 70536bf4eb07 ("NFS: Clean up reset of the mirror accounting variables")
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

Signed-off-by: Steve Dickson <steved@redhat.com>

fs/nfs/pagelist.c
@@ -1227,6 +1227,7 @@ static int nfs_do_recoalesce(struct nfs_pageio_descriptor *desc)
 
 	do {
 		list_splice_init(&mirror->pg_list, &head);
+		mirror->pg_recoalesce = 0;
 
 		while (!list_empty(&head)) {
 			struct nfs_page *req;