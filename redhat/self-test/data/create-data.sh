1e63ee2a42c9a2dba43e3b7377e7e167cbb03f02
Prarit Bhargava <prarit@redhat.com>
2022-04-14 09:30:10 -0400

    redhat/self-test: Add better dist-dump-variables test
    Bugzilla: INTERNAL
    
    commit 5218fb650e6d36896bc9b9421e7b5286cc93a83b
    Author: Prarit Bhargava <prarit@redhat.com>
    Date:   Fri Mar 25 20:37:06 2022 -0400
    
        redhat/self-test: Add better dist-dump-variables test
    
        The current dist-dump-variables test is not adequate in that it only tests
        a small number of fields.  Add a test to run create-data.sh and compare
        that data against the stored "known good" output in self-test/data.
    
        Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>

--- redhat/self-test/data/create-data.sh
+++ redhat/self-test/data/create-data.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
-target_dir=$1
-[ -z "${target_dir}" ] && echo "Missing target directory." && exit 1
+[ -z "${RHDISTDATADIR}" ] && echo "ERROR: RHDISTDATADIR undefined." && exit 1
 
 # This script generates 'dist-dump-variables' output for various configurations
 # using known ark commit IDs.  It uses this information as well as setting
@@ -13,21 +12,19 @@ target_dir=$1
 #    cdb70f21aa06 := kernel-5.14.0-76.el9 + 2 commits
 #
 
-[ -z "$REDHAT" ] && echo "ERROR: execution only supported through 'make dist-self-test-data'" && exit 1
-
 for DISTRO in fedora rhel centos
 do
 	for commit in 13d668e5a3f9 cdb70f21aa06
 	do
 		for DIST in .fc32 .el9
 		do
-			varfilename="${DISTRO}-${commit}${DIST}"
+			varfilename="${RHDISTDATADIR}/${DISTRO}-${commit}${DIST}"
 
 			echo "building $varfilename"
 
 			# CURDIR is a make special target and cannot be easily changed.  Omit
 			# CURDIR from the output.
-			make RHSELFTESTDATA=1 DIST="${DIST}" DISTRO="${DISTRO}" HEAD=${commit} dist-dump-variables | grep "=" | grep -v CURDIR >& "${target_dir}"/"${varfilename}"
+			make RHSELFTESTDATA=1 DIST="${DIST}" DISTRO="${DISTRO}" HEAD=${commit} dist-dump-variables | grep "=" | grep -v CURDIR >& "${varfilename}"
 
 			# When executed from a script, the variables in Makefile.variables are
 			# listed as having origin 'environment'.  This is because the script
@@ -36,7 +33,11 @@ do
 			# its output.  As a workaround, read in the variables and output them to
 			# the data file.
 			# shellcheck disable=SC2002
-			cat Makefile.variables | grep -v "^#" | sed '/^$/d' | tr -d " " | awk -F "?=|:=" '{print $1}' | while read -r VAR; do echo "$VAR=${!VAR}"; done >> "${target_dir}"/"${varfilename}"
+			cat Makefile.variables | grep -v "^#" | sed '/^$/d' | tr -d " " | awk -F "?=|:=" '{print $1}' | while read -r VAR
+			do
+				[ "$VAR" == "RHDISTDATADIR" ] && continue
+				echo "$VAR=${!VAR}"
+			done >> "${varfilename}"
 		done
 	done
 done
