bedd3eccd568510244c9fd231ebb432ef30bb973
Juri Lelli <juri.lelli@redhat.com>
2022-07-13 10:59:29 +0200

    Revert "printk: rename printk cpulock API and always disable interrupts"
    This reverts commit 45e3c6410c6eed751dbcc8c0d21c199ec1e6ea06.
    
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -294,22 +294,17 @@ static inline void printk_trigger_flush(void)
 extern int __printk_cpu_trylock(void);
 extern void __printk_wait_on_cpu_lock(void);
 extern void __printk_cpu_unlock(void);
-#else
-#define __printk_cpu_trylock()		1
-#define __printk_wait_on_cpu_lock()
-#define __printk_cpu_unlock()
-#endif /* CONFIG_SMP */
 
 /**
- * raw_printk_cpu_lock_irqsave() - Acquire the printk cpu-reentrant spinning
- *                                 lock and disable interrupts.
+ * printk_cpu_lock_irqsave() - Acquire the printk cpu-reentrant spinning
+ *                             lock and disable interrupts.
  * @flags: Stack-allocated storage for saving local interrupt state,
- *         to be passed to raw_printk_cpu_unlock_irqrestore().
+ *         to be passed to printk_cpu_unlock_irqrestore().
  *
  * If the lock is owned by another CPU, spin until it becomes available.
  * Interrupts are restored while spinning.
  */
-#define raw_printk_cpu_lock_irqsave(flags)	\
+#define printk_cpu_lock_irqsave(flags)		\
 	for (;;) {				\
 		local_irq_save(flags);		\
 		if (__printk_cpu_trylock())	\
@@ -319,15 +314,22 @@ extern void __printk_cpu_unlock(void);
 	}
 
 /**
- * raw_printk_cpu_unlock_irqrestore() - Release the printk cpu-reentrant
- *                                      spinning lock and restore interrupts.
- * @flags: Caller's saved interrupt state from raw_printk_cpu_lock_irqsave().
+ * printk_cpu_unlock_irqrestore() - Release the printk cpu-reentrant spinning
+ *                                  lock and restore interrupts.
+ * @flags: Caller's saved interrupt state, from printk_cpu_lock_irqsave().
  */
-#define raw_printk_cpu_unlock_irqrestore(flags)	\
+#define printk_cpu_unlock_irqrestore(flags)	\
 	do {					\
 		__printk_cpu_unlock();		\
 		local_irq_restore(flags);	\
-	} while (0)
+	} while (0)				\
+
+#else
+
+#define printk_cpu_lock_irqsave(flags) ((void)flags)
+#define printk_cpu_unlock_irqrestore(flags) ((void)flags)
+
+#endif /* CONFIG_SMP */
 
 extern int kptr_restrict;
 
