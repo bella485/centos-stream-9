fdc8854d43e23d8109b4ac0e90d0a61b7dc7750f
Jeffrey Layton <jlayton@redhat.com>
2022-08-22 14:26:57 -0400

    sunrpc: display the tk_runstate in debugfs output
    Signed-off-by: Jeffrey Layton <jlayton@redhat.com>

--- net/sunrpc/debugfs.c
+++ net/sunrpc/debugfs.c
@@ -30,8 +30,8 @@ tasks_show(struct seq_file *f, void *v)
 	if (task->tk_rqstp)
 		xid = be32_to_cpu(task->tk_rqstp->rq_xid);
 
-	seq_printf(f, "%5u %04x %6d 0x%x 0x%x %8ld %ps %sv%u %s a:%ps q:%s\n",
-		task->tk_pid, task->tk_flags, task->tk_status,
+	seq_printf(f, "%5u %04x %6d 0x%lx 0x%x 0x%x %8ld %ps %sv%u %s a:%ps q:%s\n",
+		task->tk_pid, task->tk_flags, task->tk_status, task->tk_runstate,
 		clnt->cl_clid, xid, rpc_task_timeout(task), task->tk_ops,
 		clnt->cl_program->name, clnt->cl_vers, rpc_proc_name(task),
 		task->tk_action, rpc_waitq);
