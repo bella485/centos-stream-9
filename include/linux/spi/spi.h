12446adab5e8059736faf589a8327108c4323abc
Al Stone <ahs3@redhat.com>
2022-12-14 17:58:56 -0700

    spi: Remove unused function spi_busnum_to_master()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2071848
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2122415
    Tested: This set has been sanity tested via simple boot tests on imx8mm
     and Jetson Xavier systems, along with the CI loop.  NB: this
     patch set is insufficient to boot Orin by itself.  Further work
     to develop tests with spidev and/or spidev_test is on-going.
    
    Conflicts:
        include/linux/spi/spi.h
    
        Context difference
    
    commit bdc7ca008e1f5539e891187032cb2cbbc3decb5e
    Author: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Date:   Thu Oct 7 14:14:13 2021 +0200
    
        spi: Remove unused function spi_busnum_to_master()
    
        The last user is gone since commit
    
                2962db71c703 ("staging/fbtft: Remove fbtft_device")
    
        in 2019.
    
        Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
        Link: https://lore.kernel.org/r/20211007121415.2401638-3-u.kleine-koenig@pengutronix.de
        Signed-off-by: Mark Brown <broonie@kernel.org>
        (cherry picked from commit bdc7ca008e1f5539e891187032cb2cbbc3decb5e)
    
    Signed-off-by: Al Stone <ahs3@redhat.com>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -761,15 +761,12 @@ extern int devm_spi_register_controller(struct device *dev,
 					struct spi_controller *ctlr);
 extern void spi_unregister_controller(struct spi_controller *ctlr);
 
-extern struct spi_controller *spi_busnum_to_master(u16 busnum);
-
 #if IS_ENABLED(CONFIG_ACPI)
 extern struct spi_device *acpi_spi_device_alloc(struct spi_controller *ctlr,
 						struct acpi_device *adev,
 						int index);
 int acpi_spi_count_resources(struct acpi_device *adev);
 #endif
-
 /*
  * SPI resource management while processing a SPI message
  */
