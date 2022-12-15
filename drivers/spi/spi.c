44d990843c3a7e35d0e3db7d368de3449e6a2bc0
Al Stone <ahs3@redhat.com>
2022-12-14 17:59:20 -0700

    spi: Move ctlr->cur_msg_prepared to struct spi_message
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2071848
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2122415
    Tested: This set has been sanity tested via simple boot tests on imx8mm
     and Jetson Xavier systems, along with the CI loop.  NB: this
     patch set is insufficient to boot Orin by itself.  Further work
     to develop tests with spidev and/or spidev_test is on-going.
    
    commit 1714582a3a087eda8786d5a1b32b2ec86ca8a303
    Author: David Jander <david@protonic.nl>
    Date:   Tue Jun 21 08:12:24 2022 +0200
    
        spi: Move ctlr->cur_msg_prepared to struct spi_message
    
        This enables the possibility to transfer a message that is not at the
        current tip of the async message queue.
        This is in preparation of the next patch(es) which enable spi_sync messages
        to skip the queue altogether.
    
        Signed-off-by: David Jander <david@protonic.nl>
        Link: https://lore.kernel.org/r/20220621061234.3626638-2-david@protonic.nl
        Signed-off-by: Mark Brown <broonie@kernel.org>
        (cherry picked from commit 1714582a3a087eda8786d5a1b32b2ec86ca8a303)
    
    Signed-off-by: Al Stone <ahs3@redhat.com>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1682,7 +1682,7 @@ static void __spi_pump_messages(struct spi_controller *ctlr, bool in_kthread)
 			spi_finalize_current_message(ctlr);
 			goto out;
 		}
-		ctlr->cur_msg_prepared = true;
+		msg->prepared = true;
 	}
 
 	ret = spi_map_msg(ctlr, msg);
@@ -1922,7 +1922,7 @@ void spi_finalize_current_message(struct spi_controller *ctlr)
 	 */
 	spi_res_release(ctlr, mesg);
 
-	if (ctlr->cur_msg_prepared && ctlr->unprepare_message) {
+	if (mesg->prepared && ctlr->unprepare_message) {
 		ret = ctlr->unprepare_message(ctlr, mesg);
 		if (ret) {
 			dev_err(&ctlr->dev, "failed to unprepare message: %d\n",
@@ -1930,9 +1930,10 @@ void spi_finalize_current_message(struct spi_controller *ctlr)
 		}
 	}
 
+	mesg->prepared = false;
+
 	spin_lock_irqsave(&ctlr->queue_lock, flags);
 	ctlr->cur_msg = NULL;
-	ctlr->cur_msg_prepared = false;
 	ctlr->fallback = false;
 	kthread_queue_work(ctlr->kworker, &ctlr->pump_messages);
 	spin_unlock_irqrestore(&ctlr->queue_lock, flags);
