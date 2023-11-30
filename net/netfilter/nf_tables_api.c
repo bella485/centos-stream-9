0cc5aa3abb112368a2c619d57f5fba04e27436df
Florian Westphal <fwestpha@redhat.com>
2023-11-30 14:24:58 +0100

    netfilter: nf_tables: out-of-bound check in chain blob
    JIRA: https://issues.redhat.com/browse/RHEL-17446
    Upstream Status: commit 08e42a0d3ad3
    
    Conflicts: net/netfilter/nf_tables_api.c
    
    Context: drop everything, cs9 does not have the expression reduction
    infrastructure.
    
    commit 08e42a0d3ad30f276f9597b591f975971a1b0fcf
    Author: Pablo Neira Ayuso <pablo@netfilter.org>
    Date:   Tue Jun 6 16:32:44 2023 +0200
    
        netfilter: nf_tables: out-of-bound check in chain blob
    
        Add current size of rule expressions to the boundary check.
    
        Fixes: 2c865a8a28a1 ("netfilter: nf_tables: add rule blob layout")
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Florian Westphal <fwestpha@redhat.com>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -9133,7 +9133,7 @@ static int nf_tables_commit_chain_prepare(struct net *net, struct nft_chain *cha
 			return -ENOMEM;
 
 		nft_rule_for_each_expr(expr, last, rule) {
-			if (WARN_ON_ONCE(data + expr->ops->size > data_boundary))
+			if (WARN_ON_ONCE(data + size + expr->ops->size > data_boundary))
 				return -ENOMEM;
 
 			memcpy(data + size, expr, expr->ops->size);
