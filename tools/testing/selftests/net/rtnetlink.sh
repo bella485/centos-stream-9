04e1d8da2aff8fa809f6ccbd7689b191e00da96b
Hangbin Liu <haliu@redhat.com>
2024-04-03 16:28:52 +0800

    kselftest: rtnetlink.sh: use grep_fail when expecting the cmd fail
    JIRA: https://issues.redhat.com/browse/RHEL-31077
    Upstream Status: net.git commit b8056f2ce07f
    
    commit b8056f2ce07f27c43b9488dd1bc8bfbb60d0779d
    Author: Hangbin Liu <liuhangbin@gmail.com>
    Date:   Tue Dec 19 14:57:37 2023 +0800
    
        kselftest: rtnetlink.sh: use grep_fail when expecting the cmd fail
    
        run_cmd_grep_fail should be used when expecting the cmd fail, or the ret
        will be set to 1, and the total test return 1 when exiting. This would cause
        the result report to fail if run via run_kselftest.sh.
    
        Before fix:
         # ./rtnetlink.sh -t kci_test_addrlft
         PASS: preferred_lft addresses have expired
         # echo $?
         1
    
        After fix:
         # ./rtnetlink.sh -t kci_test_addrlft
         PASS: preferred_lft addresses have expired
         # echo $?
         0
    
        Fixes: 9c2a19f71515 ("kselftest: rtnetlink.sh: add verbose flag")
        Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
        Reviewed-by: Simon Horman <horms@kernel.org>
        Link: https://lore.kernel.org/r/20231219065737.1725120-1-liuhangbin@gmail.com
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Hangbin Liu <haliu@redhat.com>

--- tools/testing/selftests/net/rtnetlink.sh
+++ tools/testing/selftests/net/rtnetlink.sh
@@ -284,7 +284,7 @@ kci_test_addrlft()
 	done
 
 	sleep 5
-	run_cmd_grep "10.23.11." ip addr show dev "$devdummy"
+	run_cmd_grep_fail "10.23.11." ip addr show dev "$devdummy"
 	if [ $? -eq 0 ]; then
 		end_test "FAIL: preferred_lft addresses remaining"
 		check_err 1
