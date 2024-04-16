86faebe53cf42ea51d997982d3b33092702ffeb1
Donald Dutile <ddutile@redhat.com>
2024-04-16 11:42:25 -0400

    kbuild: link symbol CRCs at final link, removing CONFIG_MODULE_REL_CRCS
    Jira: http://issues.redhat.com/28063
    
    commit 7b4537199a4a8480b8c3ba37a2d44765ce76cd9b
    Author: Masahiro Yamada <masahiroy@kernel.org>
    Date:   Fri May 13 20:39:22 2022 +0900
    
        kbuild: link symbol CRCs at final link, removing CONFIG_MODULE_REL_CRCS
    
        include/{linux,asm-generic}/export.h defines a weak symbol, __crc_*
        as a placeholder.
    
        Genksyms writes the version CRCs into the linker script, which will be
        used for filling the __crc_* symbols. The linker script format depends
        on CONFIG_MODULE_REL_CRCS. If it is enabled, __crc_* holds the offset
        to the reference of CRC.
    
        It is time to get rid of this complexity.
    
        Now that modpost parses text files (.*.cmd) to collect all the CRCs,
        it can generate C code that will be linked to the vmlinux or modules.
    
        Generate a new C file, .vmlinux.export.c, which contains the CRCs of
        symbols exported by vmlinux. It is compiled and linked to vmlinux in
        scripts/link-vmlinux.sh.
    
        Put the CRCs of symbols exported by modules into the existing *.mod.c
        files. No additional build step is needed for modules. As before,
        *.mod.c are compiled and linked to *.ko in scripts/Makefile.modfinal.
    
        No linker magic is used here. The new C implementation works in the
        same way, whether CONFIG_RELOCATABLE is enabled or not.
        CONFIG_MODULE_REL_CRCS is no longer needed.
    
        Previously, Kbuild invoked additional $(LD) to update the CRCs in
        objects, but this step is unneeded too.
    
        Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
        Tested-by: Nathan Chancellor <nathan@kernel.org>
        Tested-by: Nicolas Schier <nicolas@fjasle.eu>
        Reviewed-by: Nicolas Schier <nicolas@fjasle.eu>
        Tested-by: Sedat Dilek <sedat.dilek@gmail.com> # LLVM-14 (x86-64)
    
    Conflicts:
     1) This commit was created in v5.17 before kernel/module.c was moved to
        kernel/module/main.c.  The move-commit, upstream cfc1d277891e, was applied
        first, and resolved via upstream Merge commit ef98f9cfe after the section
        of kernel/module.c was moved to kernel/module/version.c.
        Implement the Merge by changing the affected hunks to kernel/module/version.c
        vs kernel/module.c.
     2) Similar timing and Merge issue for init/Kconfig vs kernel/module/Kconfig.
        Update kernel/module/Kconfig vs init/Kconfig.
     3) Skip Makefile.build patches;  appers RHEL9.4 version of scripts/Makefile.build
        upt to date, and excludes 5 commits (interspersed in all the backports) that
        skips 'cmd_modversions, and 'factor out genksysms command from cmd_gensymtypes,
        upstream commits: f43e31d5cb7, dc6dc3e7a73f, 8017ce50641, 7cfa2fcbac16, and
        78e9e56af.
    Signed-off-by: Donald Dutile <ddutile@redhat.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -97,7 +97,6 @@ modpost_link()
 
 		if is_enabled CONFIG_MODVERSIONS; then
 			gen_symversions
-			lds="${lds} -T .tmp_symversions.lds"
 		fi
 
 		# This might take a while, so indicate that we're doing
@@ -236,6 +235,10 @@ vmlinux_link()
 		libs="${KBUILD_VMLINUX_LIBS}"
 	fi
 
+	if is_enabled CONFIG_MODULES; then
+		objs="${objs} .vmlinux.export.o"
+	fi
+
 	if [ "${SRCARCH}" = "um" ]; then
 		wl=-Wl,
 		ld="${CC}"
@@ -361,6 +364,7 @@ cleanup()
 	rm -f vmlinux.o
 	rm -f .vmlinux.d
 	rm -f .vmlinux.objs
+	rm -f .vmlinux.export.c
 }
 
 # Use "make V=1" to debug this script
@@ -411,6 +415,10 @@ info GEN modules.builtin
 # The second line aids cases where multiple modules share the same object.
 tr '\0' '\n' < modules.builtin.modinfo | sed -n 's/^[[:alnum:]:_]*\.file=//p' |
 	tr ' ' '\n' | uniq | sed -e 's:^:kernel/:' -e 's/$/.ko/' > modules.builtin
+
+if is_enabled CONFIG_MODULES; then
+	${MAKE} -f "${srctree}/scripts/Makefile.vmlinux" .vmlinux.export.o
+fi
 
 btf_vmlinux_bin_o=""
 if is_enabled CONFIG_DEBUG_INFO_BTF; then
